5e3e56f2f158ae485a43ca8aa33606aa5a8154e2
millsmcilroy <millsmcilroy@gmail.com>
2016-01-20 15:57:14 -0700

    Added properties

--- test.js
+++ test.js
@@ -6,20 +6,21 @@
 *   different bank accounts modeling what you would find in a bank.
 *****************************************************************/
 
-var bankAccount = function (accountNumber, balance, signers, accountType) {
+var bankAccount = function (accountNumber, balance, signers, accountType, standing) {
   this.accountNumber = accountNumber;
   this.balance = balance;
   this.signers = signers;
   this.accountType = accountType;
+  this.accountInGoodStanding = standing;
 };
 
-var account12345 = new bankAccount(12345, 50.00, [ {name: "Goofy", age: 45}, {name: "Mickey", age: 37}], "checking");
+var account12345 = new bankAccount(12345, 50.00, [ {name: "Goofy", age: 45}, {name: "Mickey", age: 37}], "checking", true);
 
-console.log(account1);
+console.log(account12345);
 
-var account67891 = new bankAccount(67891, -34, [ {name: "Jim Henson", age: 74}, {name: "Big Bird", age: 69}], "savings");
+var account67891 = new bankAccount(67891, -34, [ {name: "Jim Henson", age: 74}, {name: "Big Bird", age: 69}, {name: "Count von Count", age: 1000}], "savings", false);
 
-console.log(account2);
+console.log(account67891);
 
 
 
